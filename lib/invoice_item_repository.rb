b43c9870d10c94f63c941db1d652683579913687
David Maurer <davemaurermail@gmail.com>
2015-01-22 21:25:53 -0700

    customer_repo and invoice_item_repo finished

--- lib/invoice_item_repository.rb
+++ lib/invoice_item_repository.rb
@@ -1,7 +1,52 @@
+require_relative './invoice_item_parser.rb'
+require_relative './invoice_item.rb'
 require 'pry'
-require 'csv'
 
 
-class InvoiceItemsRepository
+class InvoiceItemRepository
+
+  def initialize(filename= "./test/support/invoice_items_test.csv")
+    parser = InvoiceItemParser.new(filename)
+    @file = parser.parse
+  end
+
+  def all
+    all = []
+    @file.each do |invoice_item|
+      all << "ID: #{invoice_item.id}, ITEM ID: #{invoice_item.item_id}, INVOICE ID: #{invoice_item.invoice_id}, QUANTITY: #{invoice_item.quantity}, UNIT PRICE: #{invoice_item.unit_price}"
+    end
+    all
+  end
+
+  def random
+    random_invoice_item = @file.sample
+    "ID: #{random_invoice_item.id}, ITEM ID: #{random_invoice_item.item_id}, INVOICE ID: #{random_invoice_item.invoice_id}, QUANTITY: #{random_invoice_item.quantity}, UNIT PRICE: #{random_invoice_item.unit_price}"
+  end
+
+  def find_by_id(id_num = nil)
+    @file.each do |invoice_item|
+      if invoice_item.id == id_num
+        return "ID: #{invoice_item.id}, ITEM ID: #{invoice_item.item_id}, INVOICE ID: #{invoice_item.invoice_id}, QUANTITY: #{invoice_item.quantity}, UNIT PRICE: #{invoice_item.unit_price}"
+      end
+    end
+  end
+
+  def find_by_item_id(item_id = nil)
+    @file.each do |invoice_item|
+      if invoice_item.item_id == item_id
+        return "ID: #{invoice_item.id}, ITEM ID: #{invoice_item.item_id}, INVOICE ID: #{invoice_item.invoice_id}, QUANTITY: #{invoice_item.quantity}, UNIT PRICE: #{invoice_item.unit_price}"
+      end
+    end
+  end
+
+  def find_all_by_item_id(item_id)
+    invoice_items = []
+    @file.each do |invoice_item|
+      if invoice_item.item_id == item_id
+        invoice_items << "ID: #{invoice_item.id}, ITEM ID: #{invoice_item.item_id}, INVOICE ID: #{invoice_item.invoice_id}, QUANTITY: #{invoice_item.quantity}, UNIT PRICE: #{invoice_item.unit_price}"
+      end
+    end
+    invoice_items
+  end
 
 end
--- test/customer_repository_test.rb
+++ test/customer_repository_test.rb
@@ -5,29 +5,48 @@ require_relative '../lib/customer_repository.rb'
 class CustomerRepositoryTest < Minitest::Test
 
   def setup
-
-
+    @customer_repository = CustomerRepository.new
   end
 
   def test_it_exists?
-    customer_repository = CustomerRepository.new
-    assert customer_repository
+    assert @customer_repository
   end
 
   def test_it_has_an_all_method
+    assert @customer_repository.all
+  end
 
-
+  def test_all_method_returns_all_customers
+    expected = ["NAME: Joey Ondricka, ID: 1", "NAME: Cecelia Osinski, ID: 2", "NAME: Mariah Toy, ID: 3", "NAME: Leanne Braun, ID: 4", "NAME: Sylvester Nader, ID: 5", "NAME: Sylvester Nader, ID: 6"]
+    assert_equal expected, @customer_repository.all
   end
 
   def test_it_has_a_random_method
-
-
+    assert @customer_repository.random
   end
 
-  def this_is_a_fake_test_test
-
+  def test_it_has_find_by_id_method
+    assert @customer_repository.find_by_id
   end
 
+  def test_find_by_id_returns_correct_match
+    expected = "NAME: Cecelia Osinski, ID: 2"
+    assert_equal expected, @customer_repository.find_by_id(2)
+  end
 
+  def test_it_has_find_by_name_method
+    assert @customer_repository.find_by_name(name)
+  end
 
+  def test_find_by_name_returns_correct_match
+    assert_equal "NAME: Cecelia Osinski, ID: 2", @customer_repository.find_by_name("Cecelia Osinski")
+  end
+
+  def test_it_has_a_find_all_records_by_name_method
+    assert @customer_repository.find_all_by_name(name)
+  end
+
+  def test_find_all_by_name_returns_all_customers
+    assert_equal ["NAME: Sylvester Nader, ID: 5", "NAME: Sylvester Nader, ID: 6"], @customer_repository.find_all_by_name("Sylvester Nader")
+  end
 end

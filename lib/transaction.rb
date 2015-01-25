6578ef5b022d69e224890322034de368a06645aa
David Maurer <davemaurermail@gmail.com>
2015-01-25 16:27:52 -0700

    added transactions. all repos and tests complete. changed some test.csv's and added a test.csv, transactions_two

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -3,9 +3,10 @@ require 'csv'
 
 
 class Transaction
-  attr_reader :invoice_id, :credit_card_number, :result
+  attr_reader :id, :invoice_id, :credit_card_number, :result
 
   def initialize(data, parent)
+    @id = data[:id].to_i
     @invoice_id = data[:invoice_id].to_i
     @credit_card_number = data[:credit_card_number].to_i
     @result = data[:result]
--- lib/transaction_repository.rb
+++ lib/transaction_repository.rb
@@ -3,6 +3,50 @@ require 'csv'
 
 
 class TransactionRepository
-  
+
+  def initialize(filename = "./test/support/transactions_test.csv")
+    parser = TransactionParser.new(filename)
+    @file = parser.parse
+  end
+
+  def all
+    all = []
+    @file.each do |transaction|
+      all << "ID: #{transaction.id}, INVOICE_ID: #{transaction.invoice_id}, CREDIT_CARD_NUMBER: #{transaction.credit_card_number}, RESULT: #{transaction.result}"
+    end
+    all
+  end
+
+  def random
+    random_transaction = @file.sample
+    "ID: #{random_transaction.id}, INVOICE_ID: #{random_transaction.invoice_id}, CREDIT_CARD_NUMBER: #{random_transaction.credit_card_number}, RESULT: #{random_transaction.result}"
+  end
+
+  def find_by_id(id_num = nil)
+    @file.each do |transaction|
+      if transaction.id == id_num
+        return "ID: #{transaction.id}, INVOICE_ID: #{transaction.invoice_id}, CREDIT_CARD_NUMBER: #{transaction.credit_card_number}, RESULT: #{transaction.result}"
+      end
+    end
+  end
+
+  def find_by_invoice_id(invoice_num = nil)
+    @file.each do |transaction|
+      if transaction.invoice_id == invoice_num
+        return "ID: #{transaction.id}, INVOICE_ID: #{transaction.invoice_id}, CREDIT_CARD_NUMBER: #{transaction.credit_card_number}, RESULT: #{transaction.result}"
+      end
+    end
+  end
+
+  def find_all_by_result(result = nil)
+    results = []
+    @file.each do |transaction|
+      if transaction.result == result
+        results << "ID: #{transaction.id}, INVOICE_ID: #{transaction.invoice_id}, CREDIT_CARD_NUMBER: #{transaction.credit_card_number}, RESULT: #{transaction.result}"
+      end
+    end
+    results
+  end
+
 
 end
